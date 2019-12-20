cef0cc4e1fdb44ba03963d72d7dedd2335c6ac86
samvermeer <s.a.m.vermeer@uva.nl>
2019-12-20 11:58:27 +0100

    pylint van -3 naar +3

--- df2markov/__init__.py
+++ df2markov/__init__.py
@@ -17,7 +17,7 @@ LOGGER = logging.getLogger(__name__)
 
 class Markov():
     '''some docstring'''
-    def __init__(self, df, state_col="state", date_col="date", user_col="user", 
+    def __init__(self, df, state_col="state", date_col="date", user_col="user",
                  session_col="session"):
         self.number_of_states = df[state_col].nunique()
 
@@ -31,7 +31,7 @@ class Markov():
             S = np.matrix(S)
             data0 = df[df[user_col] == user]
             LOGGER.info("Currently creating a transition matrix for respondent: {}".format(user))
-            for session, group in df.groupby(session_col): 
+            for session, group in df.groupby(session_col):
                 data00 = data0.loc[data0[session_col] == session]
                 transitions = data00[state_col].tolist()
                 def rank(c):
@@ -71,11 +71,11 @@ class Markov():
         for k, v in edges_wts.items():
             tmp_origin, tmp_destination = k[0], k[1]
             G.add_edge(tmp_origin, tmp_destination, weight=v, label=v)
-        #pprint(G.edges(data=True)) 
+        #pprint(G.edges(data=True))
         pos = nx.drawing.nx_pydot.graphviz_layout(G, prog='dot')
         nx.draw_networkx(G, pos)
         edge_labels = {(n1, n2):d['label'] for n1, n2, d in G.edges(data=True)}
-        nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)    
+        nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
         self.draw_markov_chain = nx.drawing.nx_pydot.write_dot(G, user + '_markov_topic.dot')
 
     def get_probability_matrices(self):
@@ -83,7 +83,7 @@ class Markov():
         for user, matrix in self.transition_matrices.items():
             LOGGER.info('Currently creating a probability transition matrix for respondent {}'.format(user))
             S = matrix.tolist()
-            for row in S: 
+            for row in S:
                 n = sum(row)
                 if n > 0:
                     row[:] = [f/sum(row) for f in row]
@@ -98,42 +98,40 @@ def create_chain(df, state_col="state", date_col="date", user_col="user"):
     Takes ... as input and returns ....
     '''
 
-SAMPLEDATA = pd.DataFrame({'user': [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
-                                    1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 
-                                    3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 
-                                    3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 
-                                    5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 
-                                    5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 
-                                    7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 
-                                    7, 7, 7, 8, 8, 8, 8, 8], 
-                           'session': [1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 
-                                       2, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 
-                                       9, 9, 9, 9, 9, 9, 9, 9, 9, 10, 10, 10, 10, 
-                                       10, 11, 11, 11, 11, 11, 11, 44, 44, 45, 45, 
-                                       45, 65, 65, 66, 66, 66, 66, 66, 66, 66, 66, 
-                                       66, 66, 66, 66, 66, 66, 66, 813, 813, 813, 
-                                       6, 6, 6, 6, 8, 8, 8, 8, 13, 13, 13, 13, 13, 
-                                       13, 13, 13, 13, 19, 19, 19, 19, 19, 20, 20, 
-                                       20, 20, 20, 20, 66, 66, 68, 68, 68], 
-                           'state': ['A', 'F', 'B', 'C', 'C', 'C', 'A', 'A', 'B', 
-                                     'C', 'B', 'C', 'A', 'A', 'B', 'C', 'F', 'A', 
-                                     'B', 'F', 'C', 'B', 'D', 'F', 'A', 'A', 'C', 
-                                     'F', 'C', 'B', 'D', 'B', 'C', 'C', 'D', 'A', 
-                                     'F', 'A', 'B', 'A', 'C', 'F', 'E', 'D', 'D', 
-                                     'C', 'C', 'F', 'A', 'E', 'A', 'D', 'F', 'A', 
-                                     'F', 'B', 'C', 'C', 'D', 'A', 'A', 'B', 'C', 
-                                     'B', 'A', 'B', 'C', 'B', 'C', 'F', 'A', 'E', 
-                                     'F', 'C', 'B', 'C', 'F', 'A', 'A', 'C', 'F', 
+SAMPLEDATA = pd.DataFrame({'user': [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                                    1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2,
+                                    3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+                                    3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5,
+                                    5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+                                    5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7,
+                                    7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+                                    7, 7, 7, 8, 8, 8, 8, 8],
+                           'session': [1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+                                       2, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5,
+                                       9, 9, 9, 9, 9, 9, 9, 9, 9, 10, 10, 10, 10,
+                                       10, 11, 11, 11, 11, 11, 11, 44, 44, 45, 45,
+                                       45, 65, 65, 66, 66, 66, 66, 66, 66, 66, 66,
+                                       66, 66, 66, 66, 66, 66, 66, 813, 813, 813,
+                                       6, 6, 6, 6, 8, 8, 8, 8, 13, 13, 13, 13, 13,
+                                       13, 13, 13, 13, 19, 19, 19, 19, 19, 20, 20,
+                                       20, 20, 20, 20, 66, 66, 68, 68, 68],
+                           'state': ['A', 'F', 'B', 'C', 'C', 'C', 'A', 'A', 'B',
+                                     'C', 'B', 'C', 'A', 'A', 'B', 'C', 'F', 'A',
+                                     'B', 'F', 'C', 'B', 'D', 'F', 'A', 'A', 'C',
+                                     'F', 'C', 'B', 'D', 'B', 'C', 'C', 'D', 'A',
+                                     'F', 'A', 'B', 'A', 'C', 'F', 'E', 'D', 'D',
+                                     'C', 'C', 'F', 'A', 'E', 'A', 'D', 'F', 'A',
+                                     'F', 'B', 'C', 'C', 'D', 'A', 'A', 'B', 'C',
+                                     'B', 'A', 'B', 'C', 'B', 'C', 'F', 'A', 'E',
+                                     'F', 'C', 'B', 'C', 'F', 'A', 'A', 'C', 'F',
                                      'C', 'D', 'D', 'B', 'C', 'C', 'D', 'A', 'F',
-                                     'A', 'B', 'A', 'C', 'F', 'E', 'E', 'E', 'E', 
+                                     'A', 'B', 'A', 'C', 'F', 'E', 'E', 'E', 'E',
                                      'B', 'F', 'E', 'E', 'B', 'D', 'F'],
-                           'date': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 
-                                    15, 16, 17, 18, 19, 20, 1, 2, 3, 4, 5, 6, 7, 8, 
-                                    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 
+                           'date': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+                                    15, 16, 17, 18, 19, 20, 1, 2, 3, 4, 5, 6, 7, 8,
+                                    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
                                     15, 16, 17, 18, 19, 20, 1, 2, 3, 4, 5, 1, 2, 3,
                                     4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,
-                                    18, 19, 20, 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 
+                                    18, 19, 20, 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4,
                                     5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
                                     19, 20, 1, 2, 3, 4, 5]})
-
-
