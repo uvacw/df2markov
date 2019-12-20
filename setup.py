644b51126ef33973b0353fa3684111a05425984e
damian0604 <d.c.trilling@uva.nl>
2019-12-20 16:56:49 +0100

    fix README and setup

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 from setuptools import setup
 
 setup(
-       name='df2markov',
-       version='0.1',
-       description='A simple way to create markov chains from dataframes',
-       author='Susan Vermeer and Damian Trilling',
-       author_email='s.a.m.vermeer@uva.nl',
-       packages=['df2markov'],  
-       install_requires=['pandas'] # ,  TODO: check
-       # scripts=['bin/...']  # add in case we want to have a command line tool
-    )
+    name='df2markov',
+    version='0.1',
+    description='A simple way to create markov chains from dataframes',
+    author='Susan Vermeer and Damian Trilling',
+    author_email='s.a.m.vermeer@uva.nl',
+    url='https://github.com/uvacw/df2markov',
+    packages=['df2markov'],  
+    install_requires=['pandas', 'pydot', 'numpy', 'networkx'],
+    classifiers=['Development Status :: 3 - Alpha',
+                 'Intended Audience :: Science/Research',
+                 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+                 'Topic :: Scientific/Engineering',
+                 'Topic :: Sociology',
+                 'Programming Language :: Python :: 3'],
+    keywords = ['markov chain','timestamped data','social science'],
+)
